76a9532b7d1e2131285f6767d06150603c21f285
KrsMt-0113 <2021865415@qq.com>
2025-12-03 16:10:20 +0800

    XMU RollCall Bot

--- xmu-rollcall-cli/setup.py
+++ xmu-rollcall-cli/setup.py
@@ -7,7 +7,7 @@ long_description = (here / "README.md").read_text(encoding="utf-8")
 
 setup(
     name="xmu-rollcall-cli",
-    version="3.1.3",
+    version="3.1.4",
     packages=find_packages(),
     include_package_data=True,
 
